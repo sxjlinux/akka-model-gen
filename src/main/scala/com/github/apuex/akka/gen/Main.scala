85f0ad7122e25addf9381b73efef566bf2a8602f
Wangxy <xtwxy@hotmail.com>
2017-11-16 18:26:41 +0800

    add dependency extraction.

--- src/main/scala/com/github/apuex/akka/gen/Main.scala
+++ src/main/scala/com/github/apuex/akka/gen/Main.scala
@@ -1,6 +1,6 @@
 package com.github.apuex.akka.gen
 
-import scala.xml.Node
+import scala.xml.{Node, Text}
 
 object Main extends App {
   if (args.length == 0) {
@@ -17,6 +17,14 @@ object Main extends App {
         .foreach(x => {
           println("  " + x.label)
           println("    " + x.attribute("name"))
+        })
+    })
+
+  xml.child.filter(x => x.label == "entity")
+    .foreach(x => {
+      x.child.filter(x => x.label == "field")
+        .foreach(x => {
+          println(x.attribute("name") == Some(Text("product_id")))
         })
     })
 }
--- src/main/scala/com/github/apuex/akka/gen/util/Dependency.scala
+++ src/main/scala/com/github/apuex/akka/gen/util/Dependency.scala
@@ -1,22 +1,32 @@
 package com.github.apuex.akka.gen.util
 
-import scala.xml.Node
+import scala.collection.mutable
+import scala.xml.{Node, Text}
 
 object Dependency {
   def apply(xml: Node): Dependency = new Dependency(xml)
 }
 
 class Dependency(xml: Node) {
+  val basePackage = xml.attribute("package")
+
   def imports: Seq[String] = {
-    protobuf.map(x => {
-      x match {
-        case "Any" => "import com.google.protobuf.any.Any"
-        case "Timestamp" => "import com.google.protobuf.timestamp.Timestamp"
-        case "ByteString" => "import com.google.protobuf.ByteString"
-      }
-    })
+    protobuf.map {
+      case "Any" => "import com.google.protobuf.any.Any"
+      case "Timestamp" => "import com.google.protobuf.timestamp.Timestamp"
+      case "ByteString" => "import com.google.protobuf.ByteString"
+    } ++ entity.map(x => s"import ${basePackage}.message.${x}") ++
+      dao.map(x => s"import ${basePackage}.dao.${x}")
   }
+
   def protobuf: Seq[String] = ???
   def entity: Seq[String] = ???
-  def dao: Seq[String] = ???
+  def dao: Seq[String] = {
+    var seq: Seq[String] = mutable.ArraySeq()
+    xml.child.filter(x => x.label == "entity" && !(x.attribute("transient") == Some(Text("true"))))
+      .foreach(x => {
+        seq += x.label
+      })
+    seq
+  }
 }
