1212bba73672c5a2e61a3463f92d6d59d5cbe26e
Wangxy <xtwxy@hotmail.com>
2019-05-30 14:29:16 +0800

    remove codegen for context mapping.

--- project/plugins.sbt
+++ project/plugins.sbt
@@ -1,4 +1,5 @@
 addSbtPlugin("com.eed3si9n" % "sbt-assembly" % "0.14.9")
+addSbtPlugin("com.typesafe.sbteclipse" % "sbteclipse-plugin" % "5.2.4")
 
 
 
--- src/main/scala/com/github/apuex/akka/gen/Main.scala
+++ src/main/scala/com/github/apuex/akka/gen/Main.scala
@@ -1,6 +1,5 @@
 package com.github.apuex.akka.gen
 
-import com.github.apuex.akka.gen.contex.mapping.ContextMappingGenerator
 
 object Main extends App {
   if (args.length == 0) {
@@ -8,7 +7,6 @@ object Main extends App {
       "\tjava -jar <this jar> <arg list>")
   } else {
     args(0) match {
-      case "generate-context-mapping" => new ContextMappingGenerator(args.drop(1)(0)).generate()
       case c =>
         println(s"unknown command '${c}'")
     }
